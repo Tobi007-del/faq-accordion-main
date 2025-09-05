d0a958b76f06ff89e1c100152dfa6ba09d4d40af
Tobi-007 <tobioketade007@gmail.com>
2025-09-05 15:07:09 +0100

    refactor: prettier formatting

--- faq-accordion.js
+++ faq-accordion.js
@@ -3,33 +3,29 @@ const faqans = document.querySelectorAll(".faq-answers");
 const info = document.querySelectorAll(".plus");
 const hide = document.querySelectorAll(".minus");
 
-faqs.forEach((faq,i) => {
-    faq.addEventListener('click',()=>{
-        faqans.forEach((faqan,n)=>{
-            if(n === i){
-                faqan.classList.toggle('none');
-                if(faqan.classList.contains('none')){
-                    info[n].classList.remove('none');
-                    hide[n].classList.remove('inline');
-                    info[n].classList.add('inline');
-                    hide[n].classList.add('none');
-                } else {
-                    info[n].classList.remove('inline');
-                    hide[n].classList.remove('none');
-                    info[n].classList.add('none');
-                    hide[n].classList.add('inline');
-                }
-            } else {
-                faqan.classList.add('none');
-                info[n].classList.remove('none');
-                hide[n].classList.remove('inline');
-                info[n].classList.add('inline');
-                hide[n].classList.add('none');
-            }
-        })
-    })
-})
-
-
-
-
+faqs.forEach((faq, i) => {
+  faq.addEventListener("click", () => {
+    faqans.forEach((faqan, n) => {
+      if (n === i) {
+        faqan.classList.toggle("none");
+        if (faqan.classList.contains("none")) {
+          info[n].classList.remove("none");
+          hide[n].classList.remove("inline");
+          info[n].classList.add("inline");
+          hide[n].classList.add("none");
+        } else {
+          info[n].classList.remove("inline");
+          hide[n].classList.remove("none");
+          info[n].classList.add("none");
+          hide[n].classList.add("inline");
+        }
+      } else {
+        faqan.classList.add("none");
+        info[n].classList.remove("none");
+        hide[n].classList.remove("inline");
+        info[n].classList.add("inline");
+        hide[n].classList.add("none");
+      }
+    });
+  });
+});
